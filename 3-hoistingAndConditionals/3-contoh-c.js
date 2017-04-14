87208d9488297ca98d527d55ff62a8f08c1f33d1
Nindita <nindita.harahap@gmail.com>
2017-04-14 18:01:52 +0800

    Menambahkan contoh program

--- 3-hoistingAndConditionals/3-contoh-c.js
+++ 3-hoistingAndConditionals/3-contoh-c.js
@@ -1,3 +1,8 @@
 var x = 5; // Assign 5 ke var x yang sudah dideklarasikan.
 console.log('x is ', x,' and y is ', y);
 var y = 7;
+
+var a = 3; // Assign 5 ke var x yang sudah dideklarasikan.
+var b;
+console.log('a is ', a,' and b is ', b);
+b = 6;
